754f0a77aa51bbef7acce6188d1e297b2eed0a2c
Pavlo Zhukov <pahazhukov@gmail.com>
2017-11-02 02:36:54 +0200

    Change to jsx syntax. Add bootstrap

--- src/ReactReadingTime.js
+++ src/ReactReadingTime.js
@@ -4,7 +4,9 @@ import ReactDOM  from 'react-dom';
 class ReactReadingTime extends Component {
   render() {
       return (
-          React.createElement('div', {className: 'container'}, 'Hello React!')
+          <div className='container'>
+              Hello React!
+          </div>    
       );
   }
 }
