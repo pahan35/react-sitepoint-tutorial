8eebfd9fd70e48eb6718bcea9f98f76aeaba2d44
Pavlo Zhukov <pahazhukov@gmail.com>
2017-11-02 04:01:21 +0200

    Attach functionality with to ReadingTime

--- src/ReadingTime.js
+++ src/ReadingTime.js
@@ -18,9 +18,33 @@ export default class ReadingTime extends React.Component {
         };
     }
     
+    componentWillMount() {
+        this.updateReadingTime(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.updateReadingTime(nextProps);
+    }
+
+    updateReadingTime(props) {
+        const words = this.countWords(props.text);
+        const readTime = Math.round(words / props.wordsPerMinute);
+
+        this.setState({ readTime });
+    }
+    
+    countWords(text) {
+        return text.split(/\s+/).length
+    }
+    
     render() {
         return (
-            <div></div>
+            <div>
+                <p>
+                    Estimated read time: <br/> <br/>
+                    <span>{this.state.readTime}</span>
+                </p>
+            </div>
         )
     }
 }
